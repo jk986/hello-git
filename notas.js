ad5b0a729ca82725af8947a47006a557503da0bb
jk98 <davidlrj9999@gmail.com>
2024-02-10 15:00:42 -0600

    Quito commit

---
+++ notas.js
@@ -0,0 +1,20 @@
+/*
+
+git init
+git branch -m main
+git status
+git add .
+git commit -m ''
+git config --global alias.tree 'instruc'
+git diff -> 
+git checkout archivo.* -> Me devuelve el archivo al ultimo commit guardado
+git reset -> descarta cambios y me regresa al ultimo commit hecho
+git log -> retorna registros del inicio al commit que esta en el HEAD
+git reset --hard <id> -> me regresa atras o adelante el head al commit especificado
+git reflog -> retorna registros de todo el proyecto 
+git tag -> etiquetar commits importantes por ej versiones; git tag clase_1
+
+
+*/
+
+console.log('Notasss');
